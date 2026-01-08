39ba0da3c487519f2fa21e9d735b84365a72b409
Sonali Surve <sonali.surve@rvega.co.in>
2026-01-08 13:29:51 +0530

    Updated golden

--- harness/11/Reference_text.md
+++
@@ -1,4 +0,0 @@
-```verilog
-if ((enc_p == 16'hFFFF) && (enc_c == 16'h0703))
-    $display("Ciphertext is correct");
-```
---
+++ sources/clock_divider.sv
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+module clock_divider(
+                  reset,   // input
+                  clk,     // input
+                  out_clk  // output
+                  );
+
+//port declaration
+input             reset, clk;
+output reg        out_clk;
+reg         [1:0] clk_count;
+
+always @ (posedge clk)
+   begin
+      if (!reset)
+         begin
+            clk_count <= 0;
+            out_clk   <= 0;
+         end
+      else
+         begin
+            clk_count <= clk_count + 1;
+            out_clk   <= clk_count[1];
+         end
+   end
+endmodule
