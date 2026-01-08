20eaa5ee2e01e3985c08f10395c2ce6f1bcdca20
Sonali Surve <sonali.surve@rvega.co.in>
2026-01-08 15:34:55 +0530

    Added baseline model

--- sources/clock_divider.sv
+++ sources/clock_divider.sv
@@ -1,26 +1,25 @@
-
 module clock_divider(
-                  reset                   ,//input
-                  clk                     ,//input
-                  out_clk                  //output
-                  )                       ;
-//port declaration
-input             reset, clk              ;//input declaration 
-output reg        out_clk                 ;//output declaration
-reg         [1:0] clk_count               ;//register for count
+                  reset,   // input
+                  clk,     // input
+                  out_clk  // output
+                  );
 
-/*Here the clock count is incremented for every positive edge and the out clock is taken as msb of clock count that will be equal to 2 times period of original clock*/
+//port declaration
+input             reset, clk;
+output reg        out_clk;
+reg         [1:0] clk_count;
+
 always @ (posedge clk)
    begin
-      if (reset)
+      if (!reset)
          begin
-            clk_count <= 0                ;//reset clk count
-            out_clk   <= 0                ;//reset output
+            clk_count <= 0;
+            out_clk   <= 0;
          end
       else
          begin
-            clk_count <= clk_count + 1    ;//clock count is incremented
-            out_clk   <= clk_count[1]     ;
+            clk_count <= clk_count + 1;
+            out_clk   <= clk_count[1];
          end
    end
-endmodule                                  //end of clock divider
+endmodule
